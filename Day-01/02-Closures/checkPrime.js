962351ea1f1b0a1047107cd25b1be795cb71d881
tkmagesh <tkmagesh77@gmail.com>
2015-02-22 16:42:04 +0530

    Day - 1 Complete

---
+++ Day-01/02-Closures/checkPrime.js
@@ -0,0 +1,21 @@
+/*Write a function to check if the given number is a prime number (returns true) or not (returns false). 
+
+The algorithm is invoked only once for a given number.  For all subsequent times, the result should be returned from a cache
+*/
+var isPrime = (function(){
+   var cache= {};
+   function checkPrime(n){
+      console.log("Prime check triggered");
+      if (n <= 3) return true;
+      for(var i = 2; i <= (n/2); i++)
+        if (n % i === 0) return false;
+      return true;
+   }
+
+  return function(n){ 
+     if (typeof cache[n] === "undefined") cache[n] = checkPrime(n);
+     return cache[n];
+  }
+})()
+
+var x = (1 + 2)
